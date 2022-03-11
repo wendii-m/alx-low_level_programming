cc9913d08b4f0814e094dc96b4c9ddbcdf66d512
Michelle Wendy <wendymichelle274@gmail.com>
2022-03-11 22:43:08 +0300

    update c files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,89 +1,24 @@
-#!/bin/bash 
-
 #include <stdio.h>
 
-
-
-
-
-
-
-
-
 /**
-
-
-* main - Entry point
-
-
-*
-
-
-* Return: Always 0 (Success)
-
-
-*/
-
-
-
-
-
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
-
 {
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
 
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
-char acharacter;
-
-
-int ainteger;
-
-
-long along;
-
-
-long long alonglong;
-
-
-float afloat;
-
-
-
-
-
-
-
-printf("Size of a char: %lu byte(s)
-", sizeof(acharacter));
-
-
-printf("Size of an int: %lu byte(s)
-", sizeof(ainteger));
-
-
-printf("Size of a long int: %lu byte(s)
-", sizeof(along));
-
-
-printf("Size of a long long int: %lu byte(s)
-", sizeof(alonglong));
-
-
-printf("Size of a float: %lu byte(s)
-", sizeof(afloat));
-
-
-
-
-
-
-
-return (0);
-
-
+	return (0);
 }
-
-
